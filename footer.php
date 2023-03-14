262a2f7b1fdf2ed4a95911eeb1299c21237b313c
Lee Xuan Ong <ongtan597@gmail.com>
2023-03-14 16:19:17 +0800

    footer

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="page-footer font-small bg-secondary mt-3">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2023 Copyright: Lee Xuan Ong
+    </div>
+    <!-- Copyright -->
+
+</footer>
