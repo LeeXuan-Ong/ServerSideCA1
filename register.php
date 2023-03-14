4c6228b8e1662ccdf8d9c20deac52c404d0165e3
Lee Xuan Ong <ongtan597@gmail.com>
2023-03-14 16:18:36 +0800

    add validation footer and css to register page

--- register.php
+++ register.php
@@ -7,9 +7,11 @@
 <!-- define some style elements-->
 
 <body>
-<div class="container">
+<main class="container">
+
 <h1>Register</h1>
-<form method="POST" name="register" onsubmit="return validation" action="addAccount.php" >
+    <div class="w-50">
+<form method="POST" name="register" onsubmit="return registerValidation()" action="addAccount.php" >
     <div class="form-group">
 <p>
 <label for='name'>Your Name:</label> <br>
@@ -72,89 +74,13 @@
 <input type="submit" value="Submit"><br>
     </div>
 </form>
+    </div>
 
     <button onclick="logvali()" class="btn btn-primary"> log</button>
-</div>
-<script>
-    let emailValidation = false;
-    let phoneValidation = false;
-    let ageValidation = false;
-    let passwordValidation = false;
 
-    //preset date input
-
-    function autoFillAge(){
-        var dob = document.forms["register"]["dob"].value;
-        var today = new Date();
-        var birthDate = new Date(dob);
-        document.getElementById("age").value = today.getFullYear() - birthDate.getFullYear();
-    }
-
-    function autoFillDOB(){
-        var age = document.forms["register"]["age"].value;
-        var today = new Date();
-        var birthDate = new Date();
-        birthDate.setFullYear(today.getFullYear() - age);
-        document.getElementById("dob").value = birthDate.toISOString().split('T')[0];
-    }
-
-    //function for validation
-    function validateEmail(){
-        var email = document.forms["register"]["email"].value;
-        if (!email.match(/[^\s@]+@[^\s@]+\.[^\s@]+/gi)) {
-            document.getElementById("email").style.borderColor = "red";
-            emailValidation = false;
-        } else{
-            document.getElementById("email").style.borderColor = "black";
-            emailValidation = true;
-        }
-    }
-
-    function validatePhone(){
-        var phone = document.forms["register"]["phone"].value;
-        if(!phone.match(/^[0-9]{3} [0-9]{3} [0-9]{4}$/)) {
-            document.getElementById("phone").style.borderColor = "red";
-            phoneValidation = false ;
-        } else{
-            document.getElementById("phone").style.borderColor = "black";
-            phoneValidation = true;
-        }
-    }
-
-    function validateAge(){
-        var age = document.forms["register"]["age"].value;
-        if(!age.match(/^[0-9]{2}$/)) {
-            document.getElementById("age").style.borderColor = "red";
-            ageValidation = false;
-        } else{
-            document.getElementById("age").style.borderColor = "black";
-            autoFillDOB();
-            ageValidation = true;
-        }
-    }
-
-    function validatePassword(){
-        var x = document.forms["register"]["password"].value;
-        var y = document.forms["register"]["confirmPassword"].value;
-        if (x !== y) {
-            document.getElementById("confirmpassword").style.borderColor = "red";
-            passwordValidation = false;
-        } else {
-            document.getElementById("confirmpassword").style.borderColor = "black";
-            passwordValidation = true;
-        }
-    }
-
-    function fullValidation(){
-        if(emailValidation && phoneValidation && ageValidation && passwordValidation){
-            return true;
-        } else{
-            return false;
-        }
-    }
-
-
-</script>
+</main>
+    <?php include 'footer.php'?>
+<script src="js/validation.js"></script>
 
 
 </body>
